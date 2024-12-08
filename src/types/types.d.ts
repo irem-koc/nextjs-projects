7048ef5226673a36fa65954139eaec10990ca377
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-08 16:05:23 +0300

    Add new types for SlideDataProps and ProductCardProps

---
+++ src/types/types.d.ts
@@ -0,0 +1,13 @@
+interface SlideDataProps {
+  img: string;
+  title: string;
+  mainTitle: string;
+  price: string;
+}
+interface ProductCardProps {
+  img: string;
+  title: string;
+  price: string;
+  desc: string;
+  rating: number;
+}
