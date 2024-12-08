f8b80a3831c684dcce62b31ca8fa2f265312db6b
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-09 00:20:30 +0300

    Add Testimonials component

---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,14 @@
+type Props = {};
+
+const Testimonials = (props: Props) => {
+  return (
+    <div>
+      <div className="container pt-16 pb-16 grid-col-2">
+        <h2 className="font-medium texxt-2xl pb-4"></h2>
+        <div className="grid lg:grid-cols-[300px,1fr] gap-4"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Testimonials;
