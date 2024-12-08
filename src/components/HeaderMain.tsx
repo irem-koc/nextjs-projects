f6e8cc2926e8f57db35ce2904be8bd6df7711e09
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-08 14:11:27 +0300

    first commit

---
+++ src/components/HeaderMain.tsx
@@ -0,0 +1,11 @@
+type Props = {};
+
+const HeaderMain = (props: Props) => {
+  return (
+    <div className="border-b border-gray-200">
+      <div className="container sm:flex justify-between items-ce"></div>
+    </div>
+  );
+};
+
+export default HeaderMain;
