739f9cd0da277fcab9dafc3a14f2a60c0b5bc0bc
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-08 15:13:48 +0300

    Add banner images and slide data

---
+++ src/constants/constants.ts
@@ -0,0 +1,16 @@
+export const slideData = [
+  {
+    id: 1,
+    img: "/banner-1.jpg",
+    title: " New Collection",
+    mainTitle: "New Arrival",
+    price: "$99.00",
+  },
+  {
+    id: 2,
+    img: "/banner-2.jpeg",
+    title: " New Collection",
+    mainTitle: "New Arrival",
+    price: "$199.00",
+  },
+];
