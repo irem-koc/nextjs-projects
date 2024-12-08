29cfaaa9809d1fd7006b72ef9ed1b5ebe6b94bfa
irem-koc <irem.koc1@std.yildiz.edu.tr>
2024-12-08 16:04:55 +0300

    Refactor page component to include Hero and NewProducts components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
+import NewProducts from "@/components/NewProducts";
 
 export default function Home() {
-  return <main>Hello</main>;
+  return (
+    <main>
+      <Hero />
+      <NewProducts />
+    </main>
+  );
 }
